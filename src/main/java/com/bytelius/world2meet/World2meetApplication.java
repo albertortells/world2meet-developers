63163f60fad3aa21cf3f3cb1cdfe4e6792f0fa4f
Albert Ortells i Cornellas <albertortells@omsis3.es>
2021-07-13 14:09:48 +0200

    Spring Initializr
    Creacion proyecto Spring

---
+++ src/main/java/com/bytelius/world2meet/World2meetApplication.java
@@ -0,0 +1,13 @@
+package com.bytelius.world2meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class World2meetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(World2meetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytelius/world2meet/World2meetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bytelius.world2meet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class World2meetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
